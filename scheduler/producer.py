6b156843c93cbebb289bb437c261ec2871536ee9
Sarin-Z <sarindangpao@gmail.com>
2026-08-30 18:24:45 +0700

    add: stage3 completed

---
+++ scheduler/producer.py
@@ -0,0 +1,23 @@
+import pika
+import os
+
+def produce(host, body):
+    username = os.environ.get("RABBITMQ_USER", "guest")
+    password = os.environ.get("RABBITMQ_PASS", "guest")
+    credentials = pika.PlainCredentials(username, password)
+
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host=host, credentials=credentials)
+    )
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange="jobs", exchange_type="direct")
+    channel.queue_declare(queue="router_jobs")
+    channel.queue_bind(queue="router_jobs", exchange="jobs", routing_key="check_interfaces")
+
+    channel.basic_publish(exchange="jobs", routing_key="check_interfaces", body=body)
+
+    connection.close()
+
+if __name__ == "__main__":
+    produce("localhost", "192.168.1.44")
